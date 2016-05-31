6adf05da805c34ebedc191c9d6dcf3f8cb3fc5ea
Kenvi Zhu <mrwhite@163.com>
2016-05-31 20:06:44 +0800

    add shots

---
+++ hackerrank/src/ds/H_Advanced_XAndHisSHots.java
@@ -0,0 +1,51 @@
+package ds;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/**
+ * Created by kenvi on 16/5/31.
+ */
+public class H_Advanced_XAndHisSHots {
+    static class Segment implements Comparable<Segment>{
+        int x,y;
+
+        @Override
+        public int compareTo(Segment o) {
+            if(this.x != o.x ) return this.x - o.x;
+            else return this.y - o.y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int M,N;
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+        Segment[] shots = new Segment[N];
+        Segment[] players = new Segment[M];
+
+        int x, y;
+        for (int i = 0; i < N; i ++ ) {
+            x = scanner.nextInt();
+            y = scanner.nextInt(); Segment segment = new Segment(); segment.x = x; segment.y = y; shots[i] = segment; } for (int i = 0; i < M; i ++ ) { x = scanner.nextInt();
+            y = scanner.nextInt();
+            Segment segment = new Segment();
+            segment.x = x;
+            segment.y = y;
+            players[i] = segment;
+        }
+
+        Arrays.sort(shots);
+        Arrays.sort(players);
+
+        int total = 0;
+
+        for (int i = 0; i < N; i ++) {
+            int start = shots[i].x;
+            int end = shots[i].y;
+        }
+        System.out.println(total);
+    }
+}
