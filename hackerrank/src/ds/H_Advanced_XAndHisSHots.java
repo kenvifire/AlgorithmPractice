841461ab87de0ae37de35e17af265e58eb5c765b
kenvi <mrwhite@163.com>
2016-06-02 07:02:45 +0800

    try bit

--- hackerrank/src/ds/H_Advanced_XAndHisSHots.java
+++ hackerrank/src/ds/H_Advanced_XAndHisSHots.java
@@ -1,36 +1,44 @@
 package ds;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
 /**
  * Created by kenvi on 16/5/31.
  */
 public class H_Advanced_XAndHisSHots {
-    static class Segment implements Comparable<Segment>{
+    static class City implements Comparable<City>{
         int x,y;
 
         @Override
-        public int compareTo(Segment o) {
+        public int compareTo(City o) {
             if(this.x != o.x ) return this.x - o.x;
             else return this.y - o.y;
         }
     }
+
+    static class SegTree {
+        long[] sum;
+        SegTree(int N) {
+            sum = new long[N << 2];
+        }
+        
+    }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int M,N;
         N = scanner.nextInt();
         M = scanner.nextInt();
-        Segment[] shots = new Segment[N];
-        Segment[] players = new Segment[M];
+        City[] shots = new City[N];
+        City[] players = new City[M];
 
         int x, y;
         for (int i = 0; i < N; i ++ ) {
             x = scanner.nextInt();
             y = scanner.nextInt();
-            Segment segment = new Segment();
+            City segment = new City();
             segment.x = x;
             segment.y = y;
             shots[i] = segment;
@@ -39,7 +47,7 @@ public class H_Advanced_XAndHisSHots {
         for (int i = 0; i < M; i ++ ) {
             x = scanner.nextInt();
             y = scanner.nextInt();
-            Segment segment = new Segment();
+            City segment = new City();
             segment.x = x;
             segment.y = y;
             players[i] = segment;
