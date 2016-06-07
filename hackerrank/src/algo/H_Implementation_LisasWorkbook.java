683e610957d44701c042f183ae5282ca3e6a8db1
kenvi <mrwhite@163.com>
2016-06-08 06:56:51 +0800

    finish 3 tests

---
+++ hackerrank/src/algo/H_Implementation_LisasWorkbook.java
@@ -0,0 +1,19 @@
+package algo;
+
+import java.util.Scanner;
+
+/**
+ * Created by hannahzhang on 16/6/8.
+ */
+public class H_Implementation_LisasWorkbook {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int count = 0;
+        int spec = 0;
+        for (int i = 1; i < n + 1; i++) {
+        }
+
+    }
+}
