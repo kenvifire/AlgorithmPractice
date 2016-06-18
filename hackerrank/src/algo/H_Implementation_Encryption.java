197f2a53a72aedaa872ee9d970d7d031d6169fad
kenvi <mrwhite@163.com>
2016-06-18 22:48:24 +0800

    add ency

---
+++ hackerrank/src/algo/H_Implementation_Encryption.java
@@ -0,0 +1,16 @@
+package algo;
+
+import java.util.Scanner;
+
+/**
+ * Created by hannahzhang on 16/6/18.
+ */
+public class H_Implementation_Encryption {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        StringBuilder sb = new StringBuilder(s.replaceAll(" ",""));
+        int L = sb.length();
+
+    }
+}
