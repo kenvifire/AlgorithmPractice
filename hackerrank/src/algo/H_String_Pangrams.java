4109794ade411499c3a35b26e9ed1279af4fc38d
kenvi <mrwhite@163.com>
2016-06-21 08:38:16 +0800

    add string

---
+++ hackerrank/src/algo/H_String_Pangrams.java
@@ -0,0 +1,28 @@
+package algo;
+
+import java.util.BitSet;
+import java.util.Scanner;
+
+/**
+ * Created by hannahzhang on 16/6/21.
+ */
+public class H_String_Pangrams {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine().toLowerCase();
+
+        BitSet bitSet = new BitSet(26);
+        for (int i  = 0; i < str.length(); i++) {
+            Character ch = str.charAt(i);
+            if(Character.isAlphabetic(ch) && !bitSet.get(ch - 'a')) {
+                bitSet.set(ch - 'a');
+            }
+        }
+
+        if(bitSet.cardinality() == 26) {
+            System.out.println("pangram");
+        }else {
+            System.out.println("not pangram");
+        }
+    }
+}
