29f241dacfa832cb2f6dcd409480e3d059574a4e
kenvi <mrwhite@163.com>
2016-06-21 18:28:12 +0800

    finish anagram,funnstring, gem stones

---
+++ hackerrank/src/algo/H_String_FunnyString.java
@@ -0,0 +1,33 @@
+package algo;
+
+import java.util.Scanner;
+
+/**
+ * Created by hannahzhang on 16/6/21.
+ */
+public class H_String_FunnyString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        scanner.nextLine();
+        while (T --> 0) {
+            String line = scanner.nextLine();
+            if(isFunny(line))  {
+                System.out.println("Funny");
+            }else {
+                System.out.println("Not Funny");
+            }
+        }
+    }
+    public static boolean isFunny(String string) {
+        String reverse = new StringBuilder(string).reverse().toString();
+        for (int i = 1; i < string.length() ; i++) {
+            if(Math.abs(string.charAt(i) - string.charAt(i-1))
+                    !=
+                    Math.abs(reverse.charAt(i) - reverse.charAt(i-1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
