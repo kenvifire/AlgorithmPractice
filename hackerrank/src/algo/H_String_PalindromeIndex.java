9ced2ae632b0cd3e3a0ce1051f384af6f502465a
kenvi <mrwhite@163.com>
2016-06-22 07:01:20 +0800

    add p

--- hackerrank/src/algo/H_String_PalindromeIndex.java
+++ hackerrank/src/algo/H_String_PalindromeIndex.java
@@ -12,21 +12,33 @@ public class H_String_PalindromeIndex {
         String line;
 
         scanner.nextLine();
-        while (T --> 0) {
+        while (T-- > 0) {
             line = scanner.nextLine();
-            System.out.println(isPalindrome(line, 0, line.length()-1));
+            int l = 0, r = line.length() - 1;
+            while (l < r) {
+                if(line.charAt(l) == line.charAt(r)) {
+                    l++;
+                    r--;
+                }else {
+                    break;
+                }
+            }
+            System.out.println(new StringBuilder(line).reverse());
+            if(l >= r) {
+                System.out.println(-1);
+                continue;
+            }
+
+            if(line.charAt(l+1) == line.charAt(r)) {
+                System.out.println(l);
+            }else {
+                System.out.println(r);
+            }
 
         }
 
     }
 
-    public static Integer isPalindrome(String str, int start , int end) {
-        if(str == null  || start == end || start == end+1 ){
-            return -1;
-        }else if(str.charAt(start) == str.charAt(end)) {
-           return isPalindrome(str, start+1, end-1);
-        }else {
-            return start;
-        }
-    }
 }
+
+
