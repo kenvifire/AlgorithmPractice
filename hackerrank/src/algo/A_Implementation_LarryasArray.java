6c8b3a8efcba4abaf8cd784246631f1f9c9dae97
kenvi <mrwhite@163.com>
2016-06-23 06:28:27 +0800

    finish larryas array

--- hackerrank/src/algo/A_Implementation_LarryasArray.java
+++ hackerrank/src/algo/A_Implementation_LarryasArray.java
@@ -29,20 +29,16 @@ public class A_Implementation_LarryasArray {
     }
 
     public static boolean checkArray(int[] array) {
-        int A,B,C;
-        for (int i = 0; i <= array.length - 3; i++) {
-            A = array[i];
-            B = array[i+1];
-            C = array[i+2];
-            if(A < B && B > C && A < C) {
-                return false;
-            }else if (A > B && B < C && A < C) {
-                return false;
-            }else if(A > B && B > C ) {
-                return false;
+        int count = 0;
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[j] < array[i]) {
+                    count++;
+                }
             }
 
+
         }
-        return true;
+        return count %2 == 0;
     }
 }
