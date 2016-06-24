d6e2495d0a507497cf1b9ecfea07e700f6611980
kenvi <mrwhite@163.com>
2016-06-25 06:57:45 +0800

    fix matrix rotation

--- hackerrank/src/algo/H_Implementation_MatrixRotationAlgo.java
+++ hackerrank/src/algo/H_Implementation_MatrixRotationAlgo.java
@@ -14,6 +14,7 @@ public class H_Implementation_MatrixRotationAlgo {
         N = scanner.nextInt();
         R = scanner.nextInt();
         int[][] matrix = new int[M][N];
+        int[][] dest = new int[M][N];
 
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
@@ -21,27 +22,39 @@ public class H_Implementation_MatrixRotationAlgo {
             }
         }
 
-        int x = 0, y = 0, h = M, w = N, r = 0;
-        int min = M < N ? M : N;
 
-        while (R -- > 0) {
+
+            int x = 0, y = 0, m = M, n = N;
+            int min = M < N ? M : N;
             while (x < min/2) {
-                for (int i = 0; i < 2 * (M - 1) + 2 * (N - 1) - 1; i++) {
-                    int[] index_a = getIndex(x, y, N, M, i);
-                    int[] index_b = getIndex(x, y, N, M, i + 1);
+                for (int i = 0; i < 2 * (m - 1) + 2 * (n - 1) ; i++) {
+                    int[] index_a = getIndex(x, y, n, m, i);
+                    int[] index_b = getIndex(x, y, n, m, i+R);
                     int tmp = matrix[index_a[0]][index_a[1]];
-                    matrix[index_a[0]][index_a[1]] = matrix[index_b[0]][index_b[1]];
-                    matrix[index_b[0]][index_b[1]] = tmp;
+                    dest[index_b[0]][index_b[1]] = tmp;
+//                    System.out.println(String.format("swap[%s]{%s,%s}->[%s]{%s,%s}",i,index_a[0],index_a[1],
+//                            i+R,index_b[0],index_b[1]));
                 }
                 x++;
                 y++;
+                n-=2;
+                m-=2;
+
             }
-        }
+
+
+//        }
+//        n-=2;
+//        m-=2;
+//        for (int i = 1; i < 2 * (m - 1) + 2 * (n - 1)-1; i++) {
+//            int[] index = getIndex(1,1,2,2,i + R);
+//            System.out.println(String.format("%s:{%s,%s}",i, index[0], index[1]));
+//        }
 
 
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
-                System.out.print(matrix[i][j] + " ");
+                System.out.print(dest[i][j] + " ");
             }
             System.out.println();
         }
@@ -49,27 +62,23 @@ public class H_Implementation_MatrixRotationAlgo {
 
     }
 
-    public static void rotation(int[][] matrix, int x, int y, int w, int h, int r) {
-        r = r % (2 * w + 2 * h);
 
-
-    }
 
     public static int[] getIndex(int x, int y, int w, int h, int i) {
         int[] index = new int[2];
-        i  = i % (2 * w + 2 * h);
-        if(i < w-1) {
-            index[0] = x;
-            index[1] = i;
-        }else if(i >= w-1 && i < w+h-1) {
-            index[0] = x + (i- w) + 1;
-            index[1] = y + w -1;
-        }else if(i >= w+h-1 && i < 2*w +h -2) {
-            index[0] = x + h -1;
-            index[1] = y + (2 * w +h - i ) - 3;
-        }else {
-            index[0]= x + (2*w + 2*h - i -4);
+        i  = i % (2 * (w-1) + 2 * (h -1));
+        if(i < h-1) {
+            index[0] = x + i;
             index[1] = y;
+        }else if(i >= h-1 && i < w+h-1) {
+            index[0] = x + h-1;
+            index[1] = y + i - h + 1;
+        }else if(i >= w+h-1 && i < 2*h +w -2) {
+            index[0] = x + (2*h + w  - i-3);
+            index[1] = y + w - 1;
+        }else {
+            index[0] = x;
+            index[1] = x + (2*w + 2*h - i -4);
         }
         return index;
 
