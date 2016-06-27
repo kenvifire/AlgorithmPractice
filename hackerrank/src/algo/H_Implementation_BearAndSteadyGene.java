7b4b1a41ed5a8ff4c1196fbb63c619d2d97a4d6c
kenvi <mrwhite@163.com>
2016-06-27 22:36:24 +0800

    try steady gene

--- hackerrank/src/algo/H_Implementation_BearAndSteadyGene.java
+++ hackerrank/src/algo/H_Implementation_BearAndSteadyGene.java
@@ -16,40 +16,52 @@ public class H_Implementation_BearAndSteadyGene {
 
         int i = 0, j = line.length() - 1;
         Map<Character,Integer> geneMap = new HashMap<>();
+        Map<Character,Integer>  steadyMap = new HashMap<>();
         geneMap.put('A', 0);
+        steadyMap.put('A', 0);
         geneMap.put('C', 0);
+        steadyMap.put('C', 0);
         geneMap.put('G', 0);
+        steadyMap.put('G', 0);
         geneMap.put('T', 0);
+        steadyMap.put('T', 0);
 
         int max = line.length() / 4;
 
-        while (i < line.length() -1 && isSteady(geneMap, max, line.charAt(i))) {
-            char ch = line.charAt(i);
-            Integer v = geneMap.get(line.charAt(i));
-            geneMap.put(ch, v+1);
-
-            i++;
+        for (int k = 0; k < line.length(); k++) {
+            Integer value = geneMap.get(line.charAt(k));
+            geneMap.put(line.charAt(k), value+1);
         }
+        int count = 0;
+        if(geneMap.get('A') > max) { steadyMap.put('A', geneMap.get('A') - max); count++;}
+        if(geneMap.get('C') > max) { steadyMap.put('C', geneMap.get('C') - max); count++;}
+        if(geneMap.get('G') > max) { steadyMap.put('G', geneMap.get('G') - max); count++;}
+        if(geneMap.get('T') > max) { steadyMap.put('T', geneMap.get('T') - max); count++; }
 
-        while (i < j && isSteady(geneMap, max, line.charAt(j))) {
-            char ch = line.charAt(j);
-            Integer v = geneMap.get(ch);
-            geneMap.put(ch, v+1);
-
-            j--;
-        }
-
-//        System.out.println(String.format("{%s,%s}",i,j));
-
-        if(i < j) {
-            System.out.println(j-i+1);
-        }else{
+        if(count == 0) {
             System.out.println(0);
+            return;
         }
+
+        while (i < j && (isNotSteady(geneMap,steadyMap,line.charAt(i)) ||
+                isNotSteady(geneMap, steadyMap, line.charAt(j)))) {
+
+            if(!isNotSteady(geneMap, steadyMap, line.charAt(i))) {
+                i++;
+            }
+
+            if(!isNotSteady(geneMap, steadyMap, line.charAt(j))) {
+                j--;
+            }
+        }
+
+        System.out.println(j - i);
+
     }
 
-    public static boolean isSteady(Map<Character,Integer> map, int max, char ch) {
-        return map.get(ch) < max;
+    public static boolean isNotSteady(Map<Character,Integer> geneMap,
+                                      Map<Character,Integer> steadyMap, char ch) {
+        return (geneMap.get(ch) > steadyMap.get(ch));
 
     }
 }
