24b3700e3b5cced883d29e3d1c0b4f725801f418
Kenvi Zhu <mrwhite@163.com>
2016-07-11 18:52:37 +0800

    add count sort

---
+++ hackerrank/src/algo/sort/CountingSort1.java
@@ -0,0 +1,33 @@
+package algo.sort;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Created by kenvi on 16/7/11.
+ */
+public class CountingSort1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        n = scanner.nextInt();
+        int x;
+        Map<Integer,Integer> countMap = new HashMap<>();
+        for (int i = 0; i <=99; i++) {
+            countMap.put(i, 0);
+        }
+        while (n-->0) {
+            x = scanner.nextInt();
+            Integer value = countMap.get(x);
+            countMap.put(x, value+1);
+
+        }
+
+        for (int i = 0; i <= 99; i++) {
+            System.out.print(countMap.get(i) +" ");
+        }
+
+
+    }
+}
