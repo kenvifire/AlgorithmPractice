eda2ce42f6ef4ea4b6b57a0076ecc33d98c6b96e
kenvi <mrwhite@163.com>
2018-02-05 08:45:06 -0800

    w36- Acid Naming

---
+++ hackerrank/src/contest/w36/AcidNaming.java
@@ -0,0 +1,29 @@
+package contest.w36;
+
+import java.util.Scanner;
+
+/**
+ * Created by kenvi on 2018/2/5.
+ */
+public class AcidNaming {
+    static String acidNaming(String acid_name) {
+        if (acid_name.startsWith("hydro") && acid_name.endsWith("ic")) {
+            return "non-metal acid";
+        } else if (acid_name.endsWith("ic")) {
+            return "polyatomic acid";
+        } else {
+            return "not an acid";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int a0 = 0; a0 < n; a0++){
+            String acid_name = in.next();
+            String result = acidNaming(acid_name);
+            System.out.println(result);
+        }
+        in.close();
+    }
+}
